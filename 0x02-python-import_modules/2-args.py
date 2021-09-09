8a81824283afba47ef760ef0db2aae3d66e6afd7
Juan Bocanegra <jbocane6@gmail.com>
2021-09-08 23:08:07 -0500

    file 2 v.5

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
-    total = len(argv)
+    import sys
+    total = len(sys.argv)
     if total <= 1:
         print("0 argument.")
     else:
@@ -10,4 +10,4 @@ if __name__ == "__main__":
         else:
             print("{:d} arguments:".format(total - 1))
         for i in range(1, total):
-            print("{:d}: {}".format(i, argv[i]))
+            print("{:d}: {}".format(i, sys.argv[i]))
