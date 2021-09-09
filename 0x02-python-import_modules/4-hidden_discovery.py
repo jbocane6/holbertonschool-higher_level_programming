95afe8340ea3ff5b4adea643e3c85cbb30cc41eb
Juan Bocanegra <jbocane6@gmail.com>
2021-09-08 23:11:06 -0500

    file 4 v.3

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import hidden_4
     all_dir = dir(hidden_4)
-    not_this = "__"
+    avoid = "__"
     for i in range(0, len(all_dir)):
-        if not_this not in all_dir[i]:
+        if avoid not in all_dir[i]:
             print(all_dir[i])
