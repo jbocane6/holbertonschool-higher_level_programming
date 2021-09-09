070fa23117f673578a3d48ba20bf3439eb2097d0
Juan Bocanegra <jbocane6@gmail.com>
2021-09-08 23:23:49 -0500

    file 2 v.6

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    total = len(sys.argv)
-    if total <= 1:
-        print("0 argument.")
-    else:
-        if total == 2:
-            print("{:d} argument:".format(total - 1))
-        else:
-            print("{:d} arguments:".format(total - 1))
-        for i in range(1, total):
-            print("{:d}: {}".format(i, sys.argv[i]))
+    countArgs = len(sys.argv[1:])
+    argument = "arguments:"
+    if countArgs == 0:
+        argument = argument[:-1] + "."
+    elif countArgs == 1:
+        argument = argument[:-2] + ":"
+    print("{:d} {}".format(countArgs, argument))
+
+    increment = 1
+    for arg in sys.argv[1:]:
+        print("{:d}: {}".format(increment, arg))
+        increment += 1
