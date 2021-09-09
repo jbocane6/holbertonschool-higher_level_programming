8b49d5ef797b0272db07d1cd7449be4aceee7414
Juan Bocanegra <jbocane6@gmail.com>
2021-09-08 23:05:02 -0500

    file 2 v.4

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,9 +5,9 @@ if __name__ == "__main__":
     if total <= 1:
         print("0 argument.")
     else:
-        if total > 2:
-            print("{:d} arguments:".format(total - 1))
-        else:
+        if total == 2:
             print("{:d} argument:".format(total - 1))
+        else:
+            print("{:d} arguments:".format(total - 1))
         for i in range(1, total):
             print("{:d}: {}".format(i, argv[i]))
