a0a071cf26da6c61417661a03d91963edc42d8ba
Juan Bocanegra <jbocane6@gmail.com>
2021-09-13 22:11:06 -0500

    file 10 v.1

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if my_list is None or len(my_list)== 0:
+        return None
+    newlist=[]
+    for i in range(len(my_list)):
+        newlist.append = "True" if my_list[i] % 2 == 0 else newlist.append = "False"
+    return newlist
