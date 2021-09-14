ca5f555dd494cf1a72e43b83ff9cc6d3395d653d
Juan Bocanegra <jbocane6@gmail.com>
2021-09-14 09:53:02 -0500

    file 0 v.1

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def square(number):
+    return number ** 2
+
+new_matrix = []
+def square_matrix_simple(matrix=[]):
+    for i in range(len(matrix)):
+        new_matrix.append(list(map(square, matrix[i])))
+    return new_matrix
