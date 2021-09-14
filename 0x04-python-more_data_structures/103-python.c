95f1760078f15895ea324525c02689cbd784b22c
Juan Bocanegra <jbocane6@gmail.com>
2021-09-14 15:34:20 -0500

    file 103 v.2

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,25 +1,62 @@
-
 #include <Python.h>
-/**
- * print_python_list - print list.
- * @p: pyobject.
- */
+
+void print_python_list(PyObject *p);
 void print_python_bytes(PyObject *p);
+
+/**
+ * print_python_list - print some basic info about Python lists
+ * @p: pyObject
+ */
 void print_python_list(PyObject *p)
 {
-	unsigned long int tope = ((PyVarObject *)p)->ob_size, i;
+	PyListObject *list_object = (PyListObject *)p;
+	PyVarObject *list_var = (PyVarObject *)p;
+	const char *type;
+	int size = list_var->ob_size, alloc = list_object->allocated, i;
 
 	printf("[*] Python list info\n");
-	printf("[*] Size of the Python List = %ld\n", tope);
-	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
-	for (i = 0; i < tope; i++)
-		printf("Element %ld: %s\n", i, PySequence_GetItem(p, i)->ob_type->tp_name);
+	printf("[*] Size of the Python List = %d\n", size);
+	printf("[*] Allocated = %d\n", alloc);
+
+	for (i = 0; i < size; i++)
+	{
+		type = list_object->ob_item[i]->ob_type->tp_name;
+		printf("Element %d: %s\n", i, type);
+		if (strcmp(type, "bytes") == 0)
+			print_python_bytes(list_object->ob_item[i]);
+	}
 }
+
 /**
- * print_python_bytes - print bytes.
- * @p: pyobject.
+ * print_python_bytes - print some basic info about Python byte objects.
+ * @p: pyObject
  */
 void print_python_bytes(PyObject *p)
 {
-	(void)*p;
+	unsigned char i, size;
+	PyBytesObject *bytes = (PyBytesObject *)p;
+
+	printf("[.] bytes object info\n");
+	if (strcmp(p->ob_type->tp_name, "bytes") != 0)
+	{
+		printf("  [ERROR] Invalid Bytes Object\n");
+		return;
+	}
+
+	printf("  size: %ld\n", ((PyVarObject *)p)->ob_size);
+	printf("  trying string: %s\n", bytes->ob_sval);
+
+	if (((PyVarObject *)p)->ob_size > 10)
+		size = 10;
+	else
+		size = ((PyVarObject *)p)->ob_size + 1;
+
+	printf("  first %d bytes: ", size);
+	for (i = 0; i < size; i++)
+	{
+		printf("%02hhx", bytes->ob_sval[i]);
+		if (i == (size - 1))
+			printf("\n");
+		printf(" ");
+	}
 }
