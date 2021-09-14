e78ab09b23d59f20e073ffc2d912b529dd18c7f4
Juan Bocanegra <jbocane6@gmail.com>
2021-09-14 13:36:24 -0500

    file 102 v.2

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    new_dictionary = a_dictionary.copy()
-    for k, v in new_dictionary.items():
-        if value in v:
-            del a_dictionary[k]
-    return a_dictionary
-"""copy() returns a copy of the dictionary
-   items(return key & value as a tuple) with indexes key and value"""
+    if a_dictionary:
+        for key, val in list(a_dictionary.items()):
+            if a_dictionary[key] == value:
+                del a_dictionary[key]
+    return(a_dictionary)
