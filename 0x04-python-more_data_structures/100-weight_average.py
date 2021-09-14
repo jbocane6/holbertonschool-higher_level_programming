7a26ac32d1d23494480b56b1046a0c85a824b191
Juan Bocanegra <jbocane6@gmail.com>
2021-09-14 13:08:39 -0500

    file 100 v.1

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def weight_average(my_list=[]):
+    num = 0
+    den = 0
+    for i in range(len(my_list)):
+        a, b = my_list[i]
+        num += a * b
+        den += b
+    return num / den
