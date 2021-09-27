0d982ecdf3673fe3e6dfccffc659e1bff384bb28
Juan Bocanegra <jbocane6@gmail.com>
2021-09-27 12:47:46 -0500

    file 0 v.3

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    count = 0 #Contará las veces que se cumple y existe el índice en el array
-    for i in range(x): #Utilizará i como comodín para moverse x cantidad de veces
+    count = 0
+    for i in range(x):
         try:
-            print("{}".format(my_list[i]),end="") #imprime el valor de el array como texto evitando el salto de línea
+            print("{}".format(my_list[i]),end="")
             count += 1
-        except IndexError: #si x excede la cantidad de elementos que hay en el array
-            break #Termina el loop
-    print() #imprime el salto de línea 
-    return count #devuelve la cantidad de ocasiones que se cumple el try
+        except IndexError:
+            break
+    print()
+    return count
+""" count contará las veces que se cumple y existe el índice en el array
+for i in range(x) Utilizará i como comodín para moverse x cantidad de veces
+print("{}".format(my_list[i]),end="")imprime el valor del array sin salto línea
+except IndexError: si x excede la cantidad de elementos que hay en el array
+break termina el loop
+print() sólo imprime el salto de línea
+return count devuelve la cantidad de ocasiones que se cumple el try
+"""
