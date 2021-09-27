ce3405e71e309072289099b6fff7505218a5fdb9
Juan Bocanegra <jbocane6@gmail.com>
2021-09-27 14:13:29 -0500

    file 0 v.9999999

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,6 +7,5 @@ def safe_print_list(my_list=[], x=0):
             count += 1
     except IndexError:
         print("",end="")
-    finally:
-        print("\n",end="")
-        return count
+    print("\n",end="")
+    return count
