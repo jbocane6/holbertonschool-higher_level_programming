80fbf4215dad39130ab3da1a400246182f17adef
Juan Bocanegra <jbocane6@gmail.com>
2021-09-28 15:49:57 -0500

    file 1 with comments

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""1-square
+Write a class Square that defines a square"""
+
+
 class Square:
+    """Instantiation with size (no type/value verification)"""
     def __init__(self, size):
+        """Private instance attribute: size"""
         self.__size = size
