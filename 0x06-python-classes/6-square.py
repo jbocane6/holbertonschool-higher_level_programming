8832f101210e4bc39977a68d4d48810967efe1b7
Juan Bocanegra <jbocane6@gmail.com>
2021-09-28 17:59:08 -0500

    file 6 with comments

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -22,19 +22,19 @@ class Square:
         return self.__size
 
     @size.setter
-    def size(self, size):
+    def size(self, value):
         """Setter of size.
         Args:
-            size (int): Size of the square.
+            value (int): Size of the square.
         Raises:
             TypeError: Size must be an integer.
             ValueError: if size is less than 0.
         """
-        if type(size) != int:
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = value
 
     @property
     def position(self):
