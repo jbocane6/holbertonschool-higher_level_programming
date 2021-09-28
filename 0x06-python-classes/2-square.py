ac2fe755e14a53ca7b91bb1eba1fa59f3ce0e800
Juan Bocanegra <jbocane6@gmail.com>
2021-09-28 15:53:28 -0500

    file 2 with comments

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
+"""2-square
+Write a class Square that defines a square"""
+
+
 class Square:
+    """Instantiation with optional size: def __init__(self, size=0):"""
     def __init__(self, size = 0):
+        """Size must be an integer"""
         if type(size) != int:
+            """Otherwise raise a TypeError exception with the message"""
             raise TypeError("size must be an integer")
+        """If size is less than 0"""
         if size < 0:
+            """Raise a ValueError exception with the message"""
             raise ValueError("size must be >= 0")
+        """Private instance attribute: size"""
         self.__size = size
