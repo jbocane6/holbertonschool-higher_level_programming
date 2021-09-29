568d6acfa4987e51a4e6237ba7ba2a4af0bb29c5
Juan Bocanegra <jbocane6@gmail.com>
2021-09-28 20:59:01 -0500

    file 6 with comments

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -58,14 +58,13 @@ class Square:
 
     def my_print(self):
         """Prints a square of #'s"""
-        if self.__size == 0:
-            print("")
-        else:
+        if self.__size != 0:
             """if self.__size != 0 prints a new line"""
-            if self.__position[1] > 0:
-                for i in range(self.__position[1]):
-                    print("")
-            """Prints a new square made by spaces and #"""
+            for i in range(self.__position[1]):
+                print("")
             for j in range(self.__size):
+                """Prints a new square made by spaces and #"""
                 print("{}{}".format(" " * self.__position[0],
                                     "#" * self.__size))
+        else:
+            print("")
