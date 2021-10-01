83ce5f9d5b8abf5decef850fc954c71c011f0e71
Juan Bocanegra <jbocane6@gmail.com>
2021-09-30 19:11:12 -0500

    file 19 v.2

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-"""copy_list: function that returns a copy of a list.
-"""
 def copy_list(l):
-    """m is equal but not the sam as l
-    """
-    m = list(l)
-    return m
+    return list(l)
