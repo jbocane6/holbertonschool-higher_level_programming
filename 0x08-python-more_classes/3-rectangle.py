6c43e66c93ef6f854a4f30155cd6a35051def9ef
Juan Bocanegra <jbocane6@gmail.com>
2021-10-01 21:34:09 -0500

    file

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -43,7 +43,7 @@ class Rectangle:
         return self.__width * 2 + self.__height * 2
 
     def __str__(self):
-        """Stores a square of #'s and then return it as a string to method print"""
+        """Stores a square of #'s and then return it as a string"""
         printstr = ""
         if self.__width == 0 or self.__height == 0:
             """if self.__width and self.__height == 0 stores a new line"""
