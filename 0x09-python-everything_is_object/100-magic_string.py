549caf582da6abfc4cd58514f09edce36f891383
Juan Bocanegra <jbocane6@gmail.com>
2021-10-01 23:10:51 -0500

    file

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    return("{}".format("BestSchool"))
+    magic_string.counter = getattr(magic_string, 'counter', 0) + 1
+    return ", ".join(["BestSchool" for i in range(magic_string.counter)])
