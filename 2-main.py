589a48f9a25e1a2f9faa2d9d03c679ed5c47522d
Juan Bocanegra <jbocane6@gmail.com>
2021-10-06 22:52:36 -0500

    0x07

--- 2-main.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-matrix_divided = __import__('2-matrix_divided').matrix_divided
-
-matrix = [
-        [1, 2],
-        [3, float('inf')]
-    ]
-print(matrix_divided(matrix, -2))
-print(matrix)
