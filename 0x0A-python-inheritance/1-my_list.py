567e3076ab29f19bc72146092179e07d976946a3
Juan Bocanegra <jbocane6@gmail.com>
2021-10-12 00:07:02 -0500

    file 1 v.4

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -2,8 +2,9 @@
 """Write a class MyList that inherits from list."""
 
 
-class Mylist(list):
+class MyList(list):
     """Class MyList that inherits from list."""
+
     def print_sorted(self):
         """Prints the list, but sorted."""
         print(sorted(self))
