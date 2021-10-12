db12bc3339ac839c7717add15eb8ad56cb919753
Juan Bocanegra <jbocane6@gmail.com>
2021-10-11 22:46:04 -0500

    file 6 v.1

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Write a class BaseGeometry."""
+
+
+class BaseGeometry():
+    """Class BaseGeometry."""
+    def area(self):
+        """purpose of the function
+        Args:
+            a (int): type of term.
+        Return:
+            type of return int: what return
+        Raises:
+            Type of error: message of error
+        """
+        raise Exception("area() is not implemented")
