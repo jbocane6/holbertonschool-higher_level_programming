65810ef839f1a07621e8d75ca5054f55650baf17
Juan Bocanegra <jbocane6@gmail.com>
2021-10-12 19:10:04 -0500

    file 0 v.1

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Write a function that reads a text file (UTF8) and prints it to stdout."""
+
+
+def read_file(filename=""):
+    """Reads a text file (UTF8) and prints it.
+    Args:
+        filename: Path and name of file.
+    """
+    with open(filename) as f:
+        for line in f:
+            print("{}".format(line),end="")
