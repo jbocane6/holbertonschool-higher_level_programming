eb08de4cc54e9975b0e48ff4549ce1900963b636
Juan Bocanegra <jbocane6@gmail.com>
2021-11-23 21:27:17 -0500

    file 9 v.3

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS second_table(
     id INT,
     name VARCHAR(256),
     score INT
-);
+)
 INSERT INTO second_table VALUES(1, “John”, 10);
 INSERT INTO second_table VALUES(2, “Alex”, 3);
 INSERT INTO second_table VALUES(3, “Bob”, 14);
