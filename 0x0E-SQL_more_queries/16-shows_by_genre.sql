7a2806d4a248011bbc8faf1aecbb4a57839162b3
Juan Bocanegra <jbocane6@gmail.com>
2021-11-25 23:14:31 -0500

    file 16 v.1

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,13 @@
+-- Import the database dump from hbtn_0d_tvshows to the MySQL server from this address: 
+-- https://s3.amazonaws.com/intranet-projects-files/holbertonschool-higher-level_programming+/274/hbtn_0d_tvshows.sql
+-- Script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
+-- If a show doesn’t have a genre, display NULL in the genre column.
+-- Each record should display: tv_shows.title - tv_genres.name.
+-- Results must be sorted in ascending order by the show title and genre name.
+-- You can use only one SELECT statement.
+-- The database name will be passed as an argument of the mysql command.
+SELECT tv_shows.title, tv_genres.name FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title;
+ORDER BY tv_genres.name;
