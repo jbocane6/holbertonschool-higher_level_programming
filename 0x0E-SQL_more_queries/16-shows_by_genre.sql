b24e00672811b33d47ce193f0f37caca6ee8ae0c
Juan Bocanegra <jbocane6@gmail.com>
2021-11-25 23:16:29 -0500

    file 16 v.2

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -7,7 +7,7 @@
 -- You can use only one SELECT statement.
 -- The database name will be passed as an argument of the mysql command.
 SELECT tv_shows.title, tv_genres.name FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 ORDER BY tv_shows.title;
 ORDER BY tv_genres.name;
