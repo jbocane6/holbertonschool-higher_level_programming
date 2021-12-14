bfecdee6978208f9d1849df07321c2dbfb9c3c65
Juan Bocanegra <jbocane6@gmail.com>
2021-12-13 23:52:57 -0500

    files 100 101 102 v.1

---
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Function that executes x times a function.
+// The function must be visible from outside.
+// Prototype: function (x, theFunction).
+// You are not allowed to use var.
+exports.callMeMoby = function (x, theFunction) {
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
+};
---
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Function that increments and calls a function.
+// The function must be visible from outside.
+// Prototype: function (number, theFunction).
+// You are not allowed to use var.
+exports.addMeMaybe = function (number, theFunction) {
+  theFunction(number + 1);
+};
