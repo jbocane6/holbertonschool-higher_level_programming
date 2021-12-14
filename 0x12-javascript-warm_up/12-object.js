dada548cb7586b9111b26e8da15493d8c5439fde
Juan Bocanegra <jbocane6@gmail.com>
2021-12-13 23:42:40 -0500

    files 12 13 v.1

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Update the script to replace the value 12 with 89.
+// You are not allowed to use var.
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.value = 89;
+console.log(myObject);
---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// Function that returns the addition of 2 integers.
+// The function must be visible from outside.
+// The name of the function must be add.
+// You are not allowed to use var.
+exports.add = (a, b) => parseInt(a) + parseInt(b);
