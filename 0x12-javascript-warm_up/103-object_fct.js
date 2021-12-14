17b0465c5061b21faecc349f1cba2e0adc93f7c6
Juan Bocanegra <jbocane6@gmail.com>
2021-12-13 23:56:35 -0500

    file 103 v.1

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+// Update the script by adding a new function incr that increments the integer value.
+// You are not allowed to use var
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.incr = function () {
+  return myObject.value + 1;
+};
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
