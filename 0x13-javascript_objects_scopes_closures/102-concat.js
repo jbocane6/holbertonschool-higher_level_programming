4167c87528e5e9d784d87dbbb1110e9bf6a017ec
Juan Bocanegra <jbocane6@gmail.com>
2021-12-14 22:58:14 -0500

    file 102 v.1

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -3,3 +3,14 @@
 // The first argument is the file path of the first source file.
 // The second argument is the file path of the second source file.
 // The third argument is the file path of the destination.
+let { argv } = require('process');
+const fs = require('fs');
+
+argv = argv.slice(2);
+const argc = argv.length;
+
+if (argc === 3) {
+  const fileA = fs.readFileSync(argv[0]);
+  const fileB = fs.readFileSync(argv[1]);
+  fs.writeFileSync(argv[2], fileA + fileB);
+}
