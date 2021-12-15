9f4684a6af96a29f3e58945667ecf2d01af0baf6
Juan Bocanegra <jbocane6@gmail.com>
2021-12-14 20:48:58 -0500

    files 0 1 2 v.1

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,3 +1,5 @@
 #!/usr/bin/node
 // Write an empty class Rectangle that defines a rectangle.
 // You must use the class notation for defining your class.
+class Rectangle {}
+module.exports = Rectangle;
