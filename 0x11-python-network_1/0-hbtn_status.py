8de527c2c1f31895c44fccc25577b119a4c010b8
Juan Bocanegra <jbocane6@gmail.com>
2022-01-09 17:16:02 -0500

    [UPDATE] file 0 v.2

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import urllib.request
+from urllib import request
 """
 Python script that fetches https://intranet.hbtn.io/status
 You must use the package urllib
@@ -9,10 +9,10 @@ like the following example (tabulation before -)
 You must use a with statement
 """
 
-
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    html = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(html)))
-    print("\t- content: {}".format(html))
-    print("\t- utf8 content: {}".format(html.decode('utf-8')))
+if __name__ == "__main__":
+    with request.urlopen('https://intranet.hbtn.io/status') as f:
+        response = f.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(response)))
+        print("\t- content: {}".format(response))
+        print("\t- utf8 content: {}".format(response.decode('utf-8')))
