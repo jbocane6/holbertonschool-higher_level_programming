4cffc47f783ce87418c1ff318b91fce7b8f3655e
Juan Bocanegra <jbocane6@gmail.com>
2022-01-09 15:31:28 -0500

    [UPDATE] file 6 v.1

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Function that finds a peak in a list of unsorted integers.
+"""
+
+
+def find_peak(list_of_integers):
+    """
+    Function that finds a peak in a list of unsorted integers.
+    """
+    size = len(list_of_integers)
+    if (type(list_of_integers) != list or size < 1):
+        return None
+    return peak_cmp(list_of_integers, size)
+
+def peak_cmp(list_of_integers, size):
+    """Use recursion to find peak"""
+    if size == 1:
+        return list_of_integers[0]
+    if size == 2:
+        return max(list_of_integers)
+    
+    mid_size = int(size / 2)
+    peak = list_of_integers[mid_size]
+    peak_left = list_of_integers[mid_size - 1]
+    peak_right = list_of_integers[mid_size + 1]
+
+    if peak_left < peak > peak_right:
+        return peak
+    elif (peak_left > peak):
+        return peak_cmp(int_list[:mid_size], mid_size)
+    else:
+        return peak_cmp(list_of_integers[mid_size + 1:], len(list_of_integers[mid_size + 1:]))
