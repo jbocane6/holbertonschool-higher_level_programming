39239bc20464c0a8af7046d57e88f2ecfae41c91
Juan Bocanegra <jbocane6@gmail.com>
2022-01-09 22:57:44 -0500

    [UPDATE] file 10 v.12

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,22 +8,25 @@ def find_peak(list_of_integers):
     """
     Function that finds a peak in a list of unsorted integers.
     """
-    if (type(list_of_integers) != list or len(list_of_integers) < 1):
+    if (type(list_of_integers) != list or len(list_of_integers) == 0):
         return None
     return peak_cmp(list_of_integers, len(list_of_integers))
 
 
 def peak_cmp(list_of_integers, size):
     """Use recursion to find peak"""
-    if size == 1 or size == 2:
+    if size <= 2:
         return max(list_of_integers)
 
     mid_size = int(size / 2)
     peak = list_of_integers[mid_size]
+    peak_left = list_of_integers[:mid_size]
+    peak_right = list_of_integers[mid_size + 1:]
 
     if list_of_integers[mid_size - 1] < peak > list_of_integers[mid_size + 1]:
         return peak
     else:
-        return max(peak_cmp(list_of_integers[:mid_size], mid_size), peak_cmp(
-                            list_of_integers[mid_size + 1:],
-                            len(list_of_integers[mid_size + 1:])))
+        return peak_cmp(peak_left, mid_size) if peak_cmp(
+                        peak_left, mid_size) > peak_cmp(
+                            peak_right, len(peak_right)) else peak_cmp(
+                                peak_right, len(peak_right))
