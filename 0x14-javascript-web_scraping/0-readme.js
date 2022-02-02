cfa1f19f3ba3547e0d0ebef577e91b2da01a729e
Juan Bocanegra <jbocane6@gmail.com>
2022-02-01 21:21:00 -0500

    [CREATE] file 0 v.1

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// Write a script that reads and prints the content of a file.
+// The first argument is the file path
+// The content of the file must be read in utf-8
+// If an error occurred during the reading, print the error object
+
+// Require fs which includes readfile() function
+const fs = require('fs');
+
+fs.readFile(process.argv[2], (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data.toString('utf-8'));
+  }
+});
