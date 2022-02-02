cb2d3cc2720ac1a850c6c3a3311a1201c22f22cb
Juan Bocanegra <jbocane6@gmail.com>
2022-02-01 22:00:59 -0500

    [UPDATE] file 2 v.2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,7 +5,11 @@
 // You must use the module request
 
 // Require https which includes get() function
-const https = require('https');
-https.get(process.argv[2], function (res) {
-  console.log('code: ', res.statusCode);
+const request = require('request');
+request(process.argv[2], (error, response) => {
+    if (error) {
+        console.log(error);
+    }else {
+        console.log('code: ', response.statusCode);
+    }
 });
