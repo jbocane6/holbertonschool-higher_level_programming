86d5fc4001c1e3e36cca5ef7f2c681dcf70af416
Juan Bocanegra <jbocane6@gmail.com>
2022-02-14 22:35:41 -0500

    [UPDATE] file 4 v.2

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,13 +1,4 @@
-const classes = ["red", "green"];
-$("#toggle_header").addClass(
-  classes[Math.floor(Math.random() * classes.length)]
-);
-$("#toggle_header").on("click", () => {
-  if ($(this).hasClass("red")) {
-    $(this).addClass("green");
-    $(this).removeClass("red");
-  } else if ($(this).hasClass("green")) {
-    $(this).addClass("red");
-    $(this).removeClass("green");
-  }
+const $ = window.jQuery;
+$("DIV#toggle_header").click(function () {
+  $("header").toggleClass("green red");
 });
